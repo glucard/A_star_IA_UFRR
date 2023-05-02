de17e61dcee3a53963c97c36f42b55e944e72eb3
George Zambonin <georgezambonin@gmail.com>
2023-05-02 19:55:46 -0400

    Last commit

--- a_star.py
+++ a_star.py
@@ -1,29 +1,59 @@
+# not safe example of a* o.o
+# python 3.11.2
+# ! pip install matplotlib
 from path_generator import paths, hDLR, p_from, p_to
 
+# put the start node in open
 open = [p_from]
+open_g = [0]
 closed = []
 
+current_path = []
+
 print(f'from {p_from} to {p_to}:')
+
 while True:
+
+    # if open is empty then exit with failure
     if len(open) == 0:
         print("ERROR")
         break
-
+    
+    # for the node that has the minimun f
     minf = float('inf')
     minf_n = None
-    for n in open:
-        for next_n, distance in paths[n].items():
-            f = distance + hDLR[next_n]
-            if f < minf and next_n not in closed:
-                minf = f
-                minf_n = next_n
-    
-    print(minf, minf_n)
-    if minf_n not in closed:
-        open.append(minf_n)
-        closed.append(minf_n)
-    if all(i_n in closed for i_n in minf_n):
-        open.remove(minf_n)
+    for i, n in enumerate(open):
+        # f(n) = g(n) + h(n)
+        f = open_g[i] + hDLR[n]
+        if f < minf and n not in closed:
+            minf = f
+            minf_n = n
+
+    # remove from open and place in closed a node n which f is minimum
+    open_g.pop(open.index(minf_n))
+    open.remove(minf_n)
+    closed.append(minf_n)
+
+    # add to current_path to keep track of the path
+    current_path.append(minf_n)
+
+    # if n is goal node then exit sucessfully
     if minf_n == p_to:
         print("SUCESSO")
         break
+    
+    # else expand n, generating all it sucessors, and attach them pointers back to n
+    for next_n, g in paths[minf_n].items():
+        open.append(next_n)
+        open_g.append(g)
+
+# solution obtained by tracking back the pointers from n to s
+solution = []
+for i in range(len(current_path)-1, 0, -1):
+    if current_path[i-1] in paths[current_path[i]]:
+        solution.insert(0, current_path[i-1])
+solution.append(p_to)
+
+
+# HERES YOUR SOLUTION, liro boi
+print(solution)
--- path_generator.py
+++ path_generator.py
@@ -1,15 +1,21 @@
 # python 3.11.2
+# ! pip install matplotlib
+
+from matplotlib import pyplot as plt
 from collections import defaultdict
 import random
+import numpy as np
+
+# params
+MIN_VISITED = 1 # min node neighbors count
+MAX_VISITED = 4 # max node neighbors count
+
+p_from = 'Brasil' # start place
+p_to = 'Japão' # destiny place
 
 
-MIN_VISITED = 1
-MAX_VISITED = 3
-
-p_from = 'Brasil'
-p_to = 'Japão'
-
-places = [
+# all places names
+places = [ 
     "Brasil",
     "Japão",
     "Canada",
@@ -18,45 +24,90 @@ places = [
     "UFRR",
     "USA",
     "Praia",
+    "Cuba",
+    "Coreia do Sul",
 ]
+
+
+# randomizing position of each place
+position = {
+    p: np.array((random.randint(0, 500), random.randint(0,500)))
+    for p in places
+}
 
 done_places = []
 avaliable_places = places.copy()
 
-random.seed(1)
-
 paths = defaultdict(dict)
+
+# creating the connections between each place
 while len(done_places) < len(places) :
+    
+    # random select the places to create a connection between nodes
     p0 = random.choice(avaliable_places)
     p1 = random.choice(avaliable_places)
     while p0 == p1:
         p1 = random.choice(avaliable_places)
     
-    distance = random.randint(1,100)
+    # calculate the distance of each point
+    distance = np.linalg.norm(position[p1] - position[p0])
+
+    # define the path and its distance
     paths[p0][p1] = distance
     paths[p1][p0] = distance
 
+    # repeated the same for each previous nodes
     for p in [p0, p1]:
+        # verify if the place already has the max neighbors count
         if len(paths[p]) >= MAX_VISITED:
+            # if it has, then remove from places that have avaliable places to new neighbors
             avaliable_places.remove(p)
+        # verify if the place already has the min neights count
         if len(paths[p]) >= MIN_VISITED:
-            if done_places.count(p) == 0:
+            # if it has, then add it to accepted (done) places
+            if p not in done_places:
                 done_places.append(p)
 
-print(paths)
-
+# defining the heuristic ditansce between a ret line from the a place to destiny place
 hDLR = defaultdict(dict)
+
+# for each place
 for p in places:
+
     distance = None
+    # if the place is the destiny, then heuristic is 0
     if p == p_to:
         distance = 0
-
+    
+    # if the place is neighbors to the destiny, then heuristic is the distance between each one
     elif p_to in paths[p].keys():
         distance = paths[p][p_to]
     
-    elif distance == None:
-        distance = random.randint(1,100)
+    # if not in previous cases, then heuristic is the distance (norm) between each place
+    else:
+        distance = np.linalg.norm(position[p] - position[p_from])
 
+    # store heuristic on its respective place
     hDLR[p] = distance
 
+
 print(hDLR)
+
+
+# just mapping the places using matplotlib for better visualization
+
+# plot places
+x, y = np.array([[x,y] for x,y in position.values()]).T
+plt.scatter(x,y)
+
+# plot connections lines between each place
+for p0 in paths:
+    for p1 in paths[p0]:
+        line_x, line_y = np.array([position[p0],position[p1]]).T
+        plt.plot(line_x, line_y, marker='o')
+
+# plot places label
+for i, txt in enumerate(places):
+    plt.annotate(txt, (x[i], y[i]))
+
+plt.show()
